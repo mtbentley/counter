9f687b6e6f654cc5ade73895c1fd3e5e70e50048
Matthew Bentley <matthew.t.bentley@gmail.com>
2014-11-14 23:02:04 -0500

    Update settings to user postgres?

--- counter/settings.py
+++ counter/settings.py
@@ -57,13 +57,23 @@ WSGI_APPLICATION = 'counter.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django',
+        'USER': "postgres",
+        'PASSWORD': "",
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
 
